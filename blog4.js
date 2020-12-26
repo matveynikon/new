373b7488427a03178c93fff222098b0e11503ce2
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-12-26 17:28:58 +0100

    Create blog4.js

---
+++ blog4.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+var k = 1;
+var z = 0;
+var v = 2000;
+var x = Math.floor(Math.random() * 6000 + 105000);
+var j = 86400000 / (v) - x;
+var l = x / 14000;
+var k = 1;
+async function start() {
+  const url = 'https://matveynikon.wixsite.com/dailyhero/post/inventions-that-help-the-world';
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox']
+  });  
+  for(var k = 1; k < 2000000; k++){
+    const page = await browser.newPage();
+    await page.waitFor(100000)
+    await page.goto(url);
+    await page.setViewport({ width: 1000, height: 900});
+    await page.waitFor(15000);
+    await page.close()  
+  }
+browser.close();
+}
+start();
