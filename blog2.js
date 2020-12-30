ea95517e0f300fc10ebac0278e3897af868b1eaa
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-12-30 13:30:15 +0100

    Update blog2.js

--- blog2.js
+++ blog2.js
@@ -14,13 +14,16 @@ async function start() {
     });  
     for(var k = 1; k < 2000; k++){
       const page = await browser.newPage();
-      await page.waitFor(100000)
+      await page.waitFor(10000)
       await page.goto(url);
       await page.setViewport({ width: 1000, height: 900});
       await page.waitFor(15000);
       await page.close()  
     }
     browser.close();
+  }
+  catch(jo){
+    console.log('jooomamaaaaa!!!!')
   }
 }
 start();
