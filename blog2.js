7e4909a43d957f7d90751880e860f5ad01639b88
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-12-30 13:18:23 +0100

    Update blog2.js

--- blog2.js
+++ blog2.js
@@ -7,18 +7,20 @@ var j = 86400000 / (v) - x;
 var l = x / 14000;
 var k = 1;
 async function start() {
-  const url = 'https://matveynikon.wixsite.com/dailyhero/post/battery-charger';
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox']
-  });  
-  for(var k = 1; k < 2000000; k++){
-    const page = await browser.newPage();
-    await page.waitFor(100000)
-    await page.goto(url);
-    await page.setViewport({ width: 1000, height: 900});
-    await page.waitFor(15000);
-    await page.close()  
+  try{
+    const url = 'https://matveynikon.wixsite.com/dailyhero/post/battery-charger';
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox']
+    });  
+    for(var k = 1; k < 2000; k++){
+      const page = await browser.newPage();
+      await page.waitFor(100000)
+      await page.goto(url);
+      await page.setViewport({ width: 1000, height: 900});
+      await page.waitFor(15000);
+      await page.close()  
+    }
+    browser.close();
   }
-browser.close();
 }
 start();
